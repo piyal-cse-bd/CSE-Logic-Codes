d6f31c4693e7f99b10a636c10ca22bd7fe6a5c00
piyal-cse-bd <mdpiyal756@gmail.com>
2026-05-07 20:37:18 +0600

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = input.nextInt();
+        System.out.print("Enter second number: ");
+        int b = input.nextInt();
+        System.out.print("Enter third number: ");
+        int c = input.nextInt();
+
+        // লজিক চেক
+        if (a >= b && a >= c) {
+            System.out.println(a + " is the largest number.");
+        } else if (b >= a && b >= c) {
+            System.out.println(b + " is the largest number.");
+        } else {
+            System.out.println(c + " is the largest number.");
+        }
+
+        input.close();
+    }
+}
